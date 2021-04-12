4adaf24156f079372fff1a45549ef97243cd051e
Gustavo Vilela <gutusmao12@hotmail.com>
2021-04-12 14:50:46 -0300

    Criação da model do projeto

---
+++ Back-End/src/main/java/br/com/sonhodepapelaria/www/model/Produto.java
@@ -0,0 +1,66 @@
+package br.com.sonhodepapelaria.www.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Produto {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private double preco;
+	
+	@NotNull
+	private String descricao;
+	
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+}
